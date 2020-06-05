786d930287400ce55038552950132a727c26878b
Deathshadowown <shanervanniekerk@gmail.com>
2020-06-05 17:12:33 +0200

    routes to login and signUp will have to change for landing page

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 
 const profileRoutes = require('./routes/userProfile');
 const homeRoutes = require('./routes/home');
+const loginRoute = require('./routes/login');
+const signUpRoute = require('./routes/signUp');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(profileRoutes);
 app.use(homeRoutes);
+app.use(loginRoute);
+app.use(signUpRoute);
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404', '404.html'));
